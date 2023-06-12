6bdf65ac2637f232bcb406f00e5d004e988ee47c
Jun <wnsgur4092@gmail.com>
2023-06-12 15:08:45 +1000

    Initial Commit

---
+++ HotelBooking/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HotelBooking
+//
+//  Created by JunHyuk Lim on 12/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by JunHyuk Lim on 12/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
