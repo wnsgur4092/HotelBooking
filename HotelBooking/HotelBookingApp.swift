4d0e12c83d6b92bf3a12481539b6f367940d9dc6
Jun <wnsgur4092@gmail.com>
2023-06-21 20:28:38 +1000

    Feat. OnBoardingView

--- HotelBooking/HotelBookingApp.swift
+++ HotelBooking/HotelBookingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HotelBookingApp: App {
     var body: some Scene {
         WindowGroup {
-            RoomCard()
+            OnBoardingView()
         }
     }
 }
---
+++ HotelBooking/OnBoardingView.swift
@@ -0,0 +1,77 @@
+//
+//  OnBoardingView.swift
+//  HotelBooking
+//
+//  Created by JunHyuk Lim on 21/6/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    //MARK: - PROPERTIES
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("OnBoarding")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(alignment: .center) {
+                    logo
+                        .padding(.vertical, 50)
+                        .padding(.horizontal, 155)
+                    
+                    mainDescription
+                        .padding(.bottom, 20)
+                        .padding(.horizontal, 32)
+                    
+                    subDescription
+                        .padding(.bottom, 30)
+                    
+                    getStartedButton
+                        .padding(.horizontal, 24)
+                        .padding(.bottom, 30)
+                }
+            }
+        }
+    }
+    
+    //MARK: - COMPONENTS
+    fileprivate var logo : some View {
+        Image("logo")
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: .infinity)
+
+    }
+    
+    fileprivate var mainDescription : some View {
+        Text("Live the high life in the breathtaking world of Melbourne. This is where you can touch, feel, and imagine.")
+            .font(.custom("Poppins-Regular", size: 24))
+            .multilineTextAlignment(.center)
+            .lineLimit(5)
+    }
+    
+    fileprivate var subDescription : some View {
+        Text("Discover our collection of your stay.")
+            .font(.custom("Poppins-Regular", size: 16))
+    }
+    
+    fileprivate var getStartedButton : some View {
+        NavigationLink {
+            HomeView()
+        } label: {
+            Text("GET STARTED")
+        }
+        .buttonStyle(PrimaryButtonStyle(font: .custom("Poppins-Reuglar", size: 16)))
+    }
+}
+
+
+//MARK: - PREVIEW
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
